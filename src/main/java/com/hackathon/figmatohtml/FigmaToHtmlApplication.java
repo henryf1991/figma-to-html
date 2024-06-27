9f9af715c5ad1e14644b35d09322b94ee51980d3
Henry Fernandes <henry.fernandes1991@gmail.com>
2024-06-27 19:45:17 +0100

    First commit

---
+++ src/main/java/com/hackathon/figmatohtml/FigmaToHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.figmatohtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigmaToHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigmaToHtmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/figmatohtml/FigmaToHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.figmatohtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigmaToHtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
